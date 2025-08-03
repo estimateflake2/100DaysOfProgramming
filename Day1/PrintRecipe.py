bcd013adcde37d5f35c0d3a8b441fccbc10c817f
100DaysOfCoding <oladipo.bankole@gmail.com>
2025-08-03 15:39:54 -0500

    Print1

--- Day1/PrintRecipe.py
+++ Day1/PrintRecipe.py
@@ -1,1 +1,3 @@
-print("Hello World")
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.\n4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
