40d87b93a4e55379613c9f61ce3899ad61263b6d
100DaysOfCoding <oladipo.bankole@gmail.com>
2025-08-30 19:44:32 -0500

    #Day25 (Part 2) Pandas & Turtle Graphics in Action 🐼🐢

---
+++ print_input.py
@@ -0,0 +1,12 @@
+#gym
+#ginish lab
+#eat
+#sleep
+activities = ["gym", "finish lab","eat", "sleep"] #
+
+
+activities.append("Cloe")
+print (activities)
+for hlkhk in activities:
+    if hlkhk == "gym":
+        print (hlkhk)
