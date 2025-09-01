19b7cbfdfc381c3a1b4f93ea0529963d36de70c6
100DaysOfCoding <oladipo.bankole@gmail.com>
2025-09-01 09:03:49 -0500

    #Day26 – List & Dictionary Comprehension + NATO Phonetic Converter

---
+++ Day 17 thru 30/Day 26/NATO-alphabet-start/main.py
@@ -0,0 +1,8 @@
+import pandas as pand
+
+df = pand.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+
+word = input (" Enter a word: ")
+translate = [phonetic_dict[letter.upper()] for letter in word]
+print (translate)
