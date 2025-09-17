efe6e97e8bb5569cffb887f34a4ce4b8babc3044
100DaysOfCoding <oladipo.bankole@gmail.com>
2025-09-17 07:06:45 -0500

    #=============== Day 32 (part 1) Email SMTP and datetime module

---
+++ Day 31 thru 50/Day 32/Birthday Wisher (Day 32) start/main.py
@@ -0,0 +1,26 @@
+from smtplib import *
+
+
+# When using gmail account you must first enable App Password:
+#     Enable 2 step verification :
+#     - Go to: myaccount.google.com → Security → 2-Step Verification
+#     - Enter Password
+#     - Turn on 2 Step Verification
+#
+#     Get App Password:
+#     - go to: https://myaccount.google.com/apppasswords
+#     - If blocked, try these steps, Go back to Security > How you sign in to Google.
+#     - Turn off “Skip password when possible.”
+#     - If it still doesn’t show: Confirm you’re using a personal Gmail account, not a Workspace account.
+
+my_email = ''
+my_password=''
+to_email = ''
+
+with SMTP("smtp.gmail.com", 587, timeout=30) as connection: #smtp(sender's hostmail,
+    connection.starttls()
+    connection.login(user= my_email, password=my_password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs=to_email,
+                        msg= 'Subject:Hello World\n\nTHis is the body of my email'
+                        )
---
+++ Day 31 thru 50/Day 32/day 32.py
@@ -0,0 +1,10 @@
+#============== Email SMTP and datetime module
+#=========== Sending emails using Python!
+#Module: Email SMTP (smtplib). Prepackaged with python and helps send email
+#        Datetime. tracks current date
+import smtplib
+#gmail's smtp host> smtp.gmail.com
+#hotmail> smtp.live.com
+#outlook> outlook.office365.com
+#yahoo> smtp.mail.yahoo.com
+# Understanding how email works
